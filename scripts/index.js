7081f5ce5e5516631095dbb7e478e576d2b540a3
thebarbakov <94574341+thebarbakov@users.noreply.github.com>
2022-01-16 16:36:00 +0300

    first

--- scripts/index.js
+++ scripts/index.js
@@ -4,30 +4,29 @@ let buttonOpenPopup = container.querySelector('.edit-button');
 let popup = container.querySelector('.popup');
 let blockName = container.querySelector('.profile__name');
 let blockJob = container.querySelector('.profile__job');
-let formSubmit = container.querySelector('.popup');
 
 function openPopup() {
     popup.classList.add('popup_opened');
     popup.classList.add('animation__open');
-}
+} //Opening Popup
 
 function removeClasses() {
     popup.classList.remove('popup_opened');
     popup.classList.remove('animation__open')
     popup.classList.remove('animation__close');
-}
+} //Removing classes to close popup
 
 function closePopup() {
     popup.classList.add('animation__close');
-    setTimeout(removeClasses,500)
-}
+    setTimeout(removeClasses,500) //animation delay
+} //Close Popup
 
 function getSetValue() {
     let inputName = document.getElementById("name-input");
     let inputJob = document.getElementById("job-input");
     inputName.setAttribute('value', blockName.textContent) 
     inputJob.setAttribute('value', blockJob.textContent) 
-}
+} //Form input values
 
 function formSubmitButton(evt) {
     evt.preventDefault();
@@ -36,12 +35,24 @@ function formSubmitButton(evt) {
     blockName.innerText = inputName.value;
     blockJob.innerText = inputJob.value;
     closePopup();
-}
+} //Form submit
 
+function setHeight(){
+    let scrollHeight = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+      );  
+    popup.style.height = scrollHeight + 'px';
+} //Height of page for popup height
+
+setHeight();
 getSetValue();
+
+window.addEventListener(`resize`, event => {setHeight();}, false);
 buttonClosePopup.addEventListener('click', closePopup);
 buttonOpenPopup.addEventListener('click', openPopup);
-formSubmit.addEventListener('submit', formSubmitButton); 
+popup.addEventListener('submit', formSubmitButton); 
 
 
 
