766e8815b4e3f4d5db3b11454595aeac83d852c6
thebarbakov <94574341+thebarbakov@users.noreply.github.com>
2022-01-20 04:25:35 +0300

    fix: ревью-1

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,19 @@
 let container = document.querySelector('.root');
 let buttonClosePopup = container.querySelector('.popup__button-close');
-let buttonOpenPopup = container.querySelector('.edit-button');
+let buttonOpenPopup = container.querySelector('.profile__button_edit-button');
 let popup = container.querySelector('.popup');
 let blockName = container.querySelector('.profile__name');
 let blockJob = container.querySelector('.profile__job');
+let inputName = document.getElementById("name-input");
+let inputJob = document.getElementById("job-input");
+let form = document.forms.popup
 
 function openPopup() {
     popup.classList.add('popup_opened');
-    popup.classList.add('animation__open');
-} //Opening Popup
-
-function removeClasses() {
-    popup.classList.remove('popup_opened');
-    popup.classList.remove('animation__open');
-    popup.classList.remove('animation__close');
-} //Removing classes to close popup
-
+} 
 function closePopup() {
-    popup.classList.add('animation__close');
-    setTimeout(removeClasses,500); //animation delay;
-} //Close Popup
-
-function getSetValue() {
-    let inputName = document.getElementById("name-input");
-    let inputJob = document.getElementById("job-input");
-    inputName.setAttribute('value', blockName.textContent);
-    inputJob.setAttribute('value', blockJob.textContent);
-} //Form input values
-
+    popup.classList.remove('popup_opened');
+}
 function formSubmitButton(evt) {
     evt.preventDefault();
     let inputName = document.getElementById("name-input");
@@ -35,24 +21,12 @@ function formSubmitButton(evt) {
     blockName.innerText = inputName.value;
     blockJob.innerText = inputJob.value;
     closePopup();
-} //Form submit
-
-function setHeight(){
-    let scrollHeight = Math.max(
-        document.body.scrollHeight, document.documentElement.scrollHeight,
-        document.body.offsetHeight, document.documentElement.offsetHeight,
-        document.body.clientHeight, document.documentElement.clientHeight
-      );  
-    popup.style.height = scrollHeight + 'px';
-} //Height of page for popup height
-
-setHeight();
-getSetValue();
-
-window.addEventListener(`resize`, event => {setHeight();}, false);
+}
+inputName.setAttribute('value', blockName.textContent);
+inputJob.setAttribute('value', blockJob.textContent);
 buttonClosePopup.addEventListener('click', closePopup);
 buttonOpenPopup.addEventListener('click', openPopup);
-popup.addEventListener('submit', formSubmitButton); 
+form.addEventListener('submit', formSubmitButton); 
 
 
 
